52596d01e6d99fcb8c6ed3b7c9381f315bd9fb0e
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-18 03:32:21 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -2,8 +2,7 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { MdDownload } from "react-icons/md";
-import { RiContactsFill } from "react-icons/ri";
+import { MdAlternateEmail, MdDownload } from "react-icons/md";
 import { SiLeetcode } from "react-icons/si";
 import RotatingRole from "./rotating-role";
 
@@ -21,12 +20,12 @@ function HeroSection() {
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
-            Hello, <br />
-            This is {' '}
+            Hello, This is{' '}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Professional `}
-            <RotatingRole roles={personalData.designations} />
-            .
+          </h1>
+          <h1 className="mt-6 text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+            {`I'm a Professional`} <br />
+            <RotatingRole roles={personalData.designations} />.
           </h1>
           <div className="my-12 flex items-center gap-5">
             <Link
@@ -47,6 +46,13 @@ function HeroSection() {
             </Link>
 
             <Link
+              href={`mailto:${personalData.email}`}
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <MdAlternateEmail size={30} />
+            </Link>
+
+            <Link
               href={personalData.leetcode}
               target='_blank'
               rel="noopener noreferrer"
@@ -57,19 +63,13 @@ function HeroSection() {
 
           </div>
           <div className="flex items-center gap-3">
-            <Link href={`mailto:${personalData.email}`} className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
-                <span>Contact me</span>
-                <RiContactsFill size={16} />
-              </button>
-            </Link>
             <Link
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               target="_blank"
               rel="noopener noreferrer"
               href={personalData.resume}
             >
-              <span>Get Resume</span>
+              <span>Resume</span>
               <MdDownload size={16} />
             </Link>
           </div>
@@ -97,7 +97,7 @@ function HeroSection() {
               <div>
                 <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
                 <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">Abu Said</span>
+                <span className="text-amber-300">Tanushree Poojary</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
               <div className="ml-4 lg:ml-8 mr-2">
