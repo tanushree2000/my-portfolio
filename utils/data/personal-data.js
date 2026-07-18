356f029d0097248d7f9ce77cdad9ef4e959d7b72
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-17 21:31:27 -0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,8 @@
 export const personalData = {
   name: "Tanushree Poojary",
   profile: '/profile.png',
-  designation: ["DevOps Engineer", "Cloud Architect", "Site Reliability Engineer", "Full Stack Developer"],
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
+  designation: ["Analytics Engineer", "Business Intelligence Analyst", "Data analyst", "Business System Analyst"],
+ description:My name is Tanushree Poojary. I am a curious and enthusiastic analyst in my daily life. I am a quick learner with a self-learning attitude. I love exploring new technologies and am passionate about problem-solving. I'm drawn to almost every stack of analytics and business intelligence, and I love making data more accessible to the world. My core skill lies in business intelligence, and I enjoy approaching most things through an analytical lens. I am available for any job opportunity that suits my skills and interests.  email: 'abusaid7388@gmail.com',
   phone: '+8801608797655',
   address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
   github: 'https://github.com/said7388',
