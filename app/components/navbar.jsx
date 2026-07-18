9aa97d58d446aa65f132bb53bb00f8b253ff7e03
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-18 03:54:08 -0500

    Refactor Navbar component structure and styles
    Updated Navbar component with new layout and logo.

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -4,15 +4,14 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="bg-transparent">
-      <div className="flex items-center justify-between py-5">
-        <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
-            TANUSHREE POOJARY
-          </Link>
-        </div>
+    return (
+    <nav className="bg-transparent z-50 relative">
+      <div className="container mx-auto flex flex-rowitems-center justify-between p-5">
+        {/* Logo */}
+        <a href="/" className="flex items-center">
+  <img src={Logo} className="w-12 h-10" alt="Logo" />
+  <span className="text-white -ml-3 text-2xl mt-3">hoomi Patani</span>
+</a>
 
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
