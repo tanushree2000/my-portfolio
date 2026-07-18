5e6678433153f044e0dc53e77190492b02ad809a
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-17 19:56:49 -0500

    Update personal data with new name and designation

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
-  name: "ABU SAID",
+  name: "Tanushree Poojary",
   profile: '/profile.png',
-  designation: "Software Developer",
+  designation: ["DevOps Engineer", "Cloud Architect", "Site Reliability Engineer", "Full Stack Developer"],
   description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'abusaid7388@gmail.com',
   phone: '+8801608797655',
@@ -14,4 +14,6 @@ export const personalData = {
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "said7388",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  
+} 
+  
