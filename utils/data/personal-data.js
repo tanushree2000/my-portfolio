152420b06b6ef825fceb56229c613c766b4af62e
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-18 02:07:35 -0500

    Update personal data by removing sensitive information
    Removed personal contact details and social media links.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,14 +4,12 @@ export const personalData = {
   designation: "Analytics Engineer",
   description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'tanushreenp.work@gmail.com',
-  phone: '+4479026327',
-  address: 'United States',
+  
   github: 'https://github.com/tanushree2000',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  
   linkedIn: 'https://www.linkedin.com/in/poojarytanushree/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  
   leetcode: "https://leetcode.com/tanushreepoojary2000/",
-  devUsername: "said7388",
+  
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
 }
