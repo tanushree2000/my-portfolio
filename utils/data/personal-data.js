379a617a8f063852729499c127c9afbfec24860e
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-18 01:13:06 -0500

    Replace personal data with new user information
    Updated personal data to reflect new user information including name, profile, designation, and contact details.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,36 +1,17 @@
 export const personalData = {
-    
-    profile: '/profilephoto.jpeg',
-    designation: "Software Developer",
-    description: `
-    <div class="text-left max-w-2xl mx-auto space-y-4">
-      
-      <p class="text-gray-300 lg:text-lg text-md leading-relaxed">
-        Hi! I’m <span class="text-pink-500 font-semibold">Bhoomi Patani ✌️ </span>, a Master’s student in Computer Science and an enthusiastic Full Stack Developer & DevOps Engineer.
-        I love crafting <strong>resilient</strong>, <strong>scalable software solutions</strong> that simplify complex problems. With a foundation in <span class="text-blue-500">cloud technologies</span>,
-        <span class="text-blue-500">microservices</span>, and <span class="text-blue-500">automation</span>, I’m passionate about optimizing workflows, securing applications, and bringing ideas to life.
-      </p>
-      <p class="text-gray-300  lg:text-lg text-md leading-relaxed">
-        💡 Whether it's building <strong>user-friendly interfaces</strong> or streamlining <strong>CI/CD pipelines</strong>, I’m driven by the power of technology to make an impact.
-        I thrive in <span class="text-green">collaborative environments</span> and am always eager to take on new challenges that push me to grow and innovate.
-      </p>
-      <p class="text-xl sm:text-lg font-semibold text-pink">Let’s connect and create something extraordinary! ✨</p>
-    </div>
-  `,
-
-export const personalData = {
-  name: "Tanushree Poojary",
-  profile: "/profile.png",
-  designation: [
-    "Analytics Engineer",
-    "Business Intelligence Analyst",
-    "Data Analyst",
-    "Business System Analyst"
-  ],
-  description: "My name is Tanushree Poojary. I am a curious and enthusiastic analyst in my daily life. I am a quick learner with a self-learning attitude. I love exploring new technologies and am passionate about problem-solving. I'm drawn to almost every stack of analytics and business intelligence, and I love making data more accessible to the world. My core skill lies in business intelligence, and I enjoy approaching most things through an analytical lens. I am available for any job opportunity that suits my skills and interests.",
-  email: "tanushreenp.work@gmail.com",
-  github: "https://github.com/tanushree2000/",
-  linkedIn: "https://www.linkedin.com/in/poojarytanushree/",
-  leetcode: "https://leetcode.com/u/tanushreepoojary2000/",
+  name: "ABU SAID",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'abusaid7388@gmail.com',
+  phone: '+8801608797655',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-};
+}
