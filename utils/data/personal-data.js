ae74ce4665b3bb55fd238f9285feb9b2a785da4a
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-17 21:45:37 -0500

    Fix description quotes in personalData object
    Updated the description field to use single quotes.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Tanushree Poojary",
   profile: '/profile.png',
   designation: ["Analytics Engineer", "Business Intelligence Analyst", "Data analyst", "Business System Analyst"],
-  description:My name is Tanushree Poojary. I am a curious and enthusiastic analyst in my daily life. I am a quick learner with a self-learning attitude. I love exploring new technologies and am passionate about problem-solving. I'm drawn to almost every stack of analytics and business intelligence, and I love making data more accessible to the world. My core skill lies in business intelligence, and I enjoy approaching most things through an analytical lens. I am available for any job opportunity that suits my skills and interests. 
+  description:'My name is Tanushree Poojary. I am a curious and enthusiastic analyst in my daily life. I am a quick learner with a self-learning attitude. I love exploring new technologies and am passionate about problem-solving. I am drawn to almost every stack of analytics and business intelligence, and I love making data more accessible to the world. My core skill lies in business intelligence, and I enjoy approaching most things through an analytical lens. I am available for any job opportunity that suits my skills and interests'. 
   email: 'tanushreenp.work@gmail.com',
   github: 'https://github.com/tanushree2000/',
   linkedIn: 'https://www.linkedin.com/in/poojarytanushree/',
