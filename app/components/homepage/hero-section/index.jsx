263a72f75206cee36aaf8f0cabcb434b10f1fca2
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-18 02:23:24 -0500

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -4,7 +4,7 @@ import { personalData } from "@/utils/data/personal-data";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
+
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
@@ -47,13 +47,7 @@ function HeroSection() {
             >
               <BsLinkedin size={30} />
             </Link>
-            <Link
-              href={personalData.facebook}
-              target='_blank'
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <FaFacebook size={30} />
-            </Link>
+     
             <Link
               href={personalData.leetcode}
               target='_blank'
@@ -61,13 +55,7 @@ function HeroSection() {
             >
               <SiLeetcode size={30} />
             </Link>
-            <Link
-              href={personalData.twitter}
-              target='_blank'
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <FaTwitterSquare size={30} />
-            </Link>
+           
           </div>
 
           <div className="flex items-center gap-3">
@@ -115,23 +103,33 @@ function HeroSection() {
               <div className="ml-4 lg:ml-8 mr-2">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`['`}</span>
-                <span className="text-amber-300">React</span>
+                <span className="text-amber-300">SQL</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NextJS</span>
+                <span className="text-amber-300">Python</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Redux</span>
+                <span className="text-amber-300">dbt</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">Express</span>
+                <span className="text-amber-300">Airflow</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">NestJS</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">MySql</span>
-                <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">MongoDB</span>
+                <span className="text-amber-300">Git</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Docker</span>
                 <span className="text-gray-400">{"', '"}</span>
-                <span className="text-amber-300">AWS</span>
+                <span className="text-amber-300">Snowflake</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">BigQuery</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Looker</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Tableau</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Collibra</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Alation</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Great Expectations</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Monte Carlo</span>
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
