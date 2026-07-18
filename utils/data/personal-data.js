95eecb7f513887b5380e6f4d4f663f4330960b64
Tanushree Poojary <56186135+tanushree2000@users.noreply.github.com>
2026-07-17 21:46:57 -0500

    Refine personal data description and formatting
    Updated the personal data description for clarity and conciseness.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,13 +1,16 @@
 export const personalData = {
   name: "Tanushree Poojary",
-  profile: '/profile.png',
-  designation: ["Analytics Engineer", "Business Intelligence Analyst", "Data analyst", "Business System Analyst"],
-  description:'My name is Tanushree Poojary. I am a curious and enthusiastic analyst in my daily life. I am a quick learner with a self-learning attitude. I love exploring new technologies and am passionate about problem-solving. I am drawn to almost every stack of analytics and business intelligence, and I love making data more accessible to the world. My core skill lies in business intelligence, and I enjoy approaching most things through an analytical lens. I am available for any job opportunity that suits my skills and interests'. 
-  email: 'tanushreenp.work@gmail.com',
-  github: 'https://github.com/tanushree2000/',
-  linkedIn: 'https://www.linkedin.com/in/poojarytanushree/',
+  profile: "/profile.png",
+  designation: [
+    "Analytics Engineer",
+    "Business Intelligence Analyst",
+    "Data Analyst",
+    "Business System Analyst"
+  ],
+  description: "My name is Tanushree Poojary. I am a curious and enthusiastic analyst with a passion for solving problems through data. I am a quick learner with a self-learning attitude, and I love exploring new technologies. I enjoy working across different areas of analytics and business intelligence, making data more accessible and actionable. My core strength lies in business intelligence, and I approach challenges through an analytical lens. I am open to opportunities where I can apply my skills, learn, and create meaningful impact.",
+  email: "tanushreenp.work@gmail.com",
+  github: "https://github.com/tanushree2000/",
+  linkedIn: "https://www.linkedin.com/in/poojarytanushree/",
   leetcode: "https://leetcode.com/u/tanushreepoojary2000/",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-  
-} 
-  
+};
